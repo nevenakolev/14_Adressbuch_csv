9e43ff9fb8f864101a500ed5d9bdcb67dc45b2e1
nevena-kolev <nevena.kolev@htl-wels.at>
2021-02-21 19:03:18 +0100

    basic GUI implementation; new class Model.java; project structure changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,10 +6,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("viewcontroller/view.fxml"));
+        primaryStage.setTitle("Phonebook");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
